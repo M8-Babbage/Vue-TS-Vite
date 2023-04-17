28bdd96222d9b66657dd548094dc9585032f8440
Edwin J. Páez <edwinjpr11@gmail.com>
2023-04-17 17:37:12 -0500

    Updates

---
+++ assets/AboutView-708234c7.js
@@ -0,0 +1,1 @@
+import{d as a,u as s,c,a as l,t as r,b as u,e as _,F as i,o as p,_ as m}from"./index-2431c1ba.js";const f=a({__name:"AboutView",setup(b){const n=s(),t=(e,o)=>{console.log(e),console.log(o)};return(e,o)=>(p(),c(i,null,[l("span",null,r(u(n).doubleCount),1),_(m,{onOnClick:t,label:"About View",color:"blue"})],64))}});export{f as default};
